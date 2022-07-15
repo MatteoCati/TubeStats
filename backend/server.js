a610080bda97f765adbd19cf5b3c1e74e059aea9
Matteo Cati <oettam.itac@gmail.com>
2022-07-16 00:28:00 +0100

    Load popular videos from youtube API

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
+require('dotenv').config()
+
 const express = require('express')
-const videosList = require('./popularVideos.json')
+const axios = require('axios').default
 
 app = express()
 
 
-app.get('/popular-videos/:country', (req, res) => {
-    console.log(req.params.country)
-    res.json(videosList)
+app.get('/popular-videos/:country', async (req, res) => {
+    
+    try {
+        const resp =  await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2cstatistics&maxResults=10&chart=mostPopular&regionCode=${req.params.country}&key=${process.env.YOUTUBE_API_KEY}`);
+        res.json(resp.data.items)
+    } catch (err) {
+        // Handle Error Here
+        console.error(req.params.country);
+        res.json({err: "invalid code "+req.params.country})
+    }
 })
 
-app.listen(4000, () => {
-    console.log('Server listining on port 4000')
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Server listening on port '+(process.env.PORT || 8080))
 })
