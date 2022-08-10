86336ea95c29defea2790614aad17c2cc6292b2a
Matteo Cati <oettam.itac@gmail.com>
2022-08-10 14:43:23 +0200

    Make top channels page responsive

--- frontend/src/pages/TopChannels.jsx
+++ frontend/src/pages/TopChannels.jsx
@@ -7,12 +7,13 @@ import './TopChannels.css'
 const TopChannelRow = ({channelInfo, position}) => {
     return (
         <div className={"channelRow " + (position%2 === 1 ? "evenRow" : "oddRow") }>
-            <div className={"channelName"}>
             <p className="number">{position}.</p>
             <img alt="Channel avatar" src={channelInfo.thumbnail.url} className="avatar"/>
-            <a href={channelInfo.url}  target="_blank" rel="noopener noreferrer">{channelInfo.title}</a>
+            <div className="channelName">
+                <a href={channelInfo.url}  target="_blank" rel="noopener noreferrer">{channelInfo.title}</a>
+                <p>{formatNumber(channelInfo.subscribers)} subscribers</p>
             </div>
-            <p>{formatNumber(channelInfo.subscribers)} subscribers</p>
+            
         </div>
     )
 }
